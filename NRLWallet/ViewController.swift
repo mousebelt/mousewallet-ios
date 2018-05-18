6cc84e9a7e1e2c245fe61e070091800fb1ed59b6
dev <dev@devs-imac.local>
2018-05-18 16:55:33 +1000

    Initial Commit

---
+++ NRLWallet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NRLWallet
+//
+//  Created by dev on 18/05/2018.
+//  Copyright © 2018 NoRestLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
